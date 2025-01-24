f69a120f70072c165c4387c2cab12f5c5457954c
Weihon79 <ervan.chin@gmail.com>
2025-01-24 17:38:04 +0100

    Balayage du default vite app; Creation du Router dans App.jsx et Creation du logements.json dans data

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
