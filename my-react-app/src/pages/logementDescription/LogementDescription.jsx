660bd1a2cbb0859a253d08c0c1094726821f6c1c
Weihon79 <ervan.chin@gmail.com>
2025-01-28 17:28:50 +0100

    logementDescription entierement fonctionnel

--- my-react-app/src/pages/logementDescription/LogementDescription.jsx
+++ my-react-app/src/pages/logementDescription/LogementDescription.jsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import RatingStars from '../../components/RatingStars/RatingStars';
+import Collapse from '../../components/collapse/Collapse';
 
 function LogementDescription({ logements }) {
   const { id } = useParams();
@@ -28,45 +29,75 @@ function LogementDescription({ logements }) {
   };
 
   return (
-    <div className="logement-description">
-        <div className="carousel">
-            <button className="carousel__button prev" onClick={prevImage}>
-                <i className="fa-solid fa-chevron-left"></i>
-            </button>
-            <img
-            src={pictures[currentImageIndex]}
-            alt={`Logement ${logement.title}`}
-            className="carousel__image"
-            />
-            <button className="carousel__button next" onClick={nextImage}>
-                <i className="fa-solid fa-chevron-right"></i>
-            </button>
+    <>
+      <div className="logement-description">
+          <div className="carousel">
+              <button className="carousel__button prev" onClick={prevImage}>
+                  <i className="fa-solid fa-chevron-left"></i>
+              </button>
+              <img
+              src={pictures[currentImageIndex]}
+              alt={`Logement ${logement.title}`}
+              className="carousel__image"
+              />
+              <button className="carousel__button next" onClick={nextImage}>
+                  <i className="fa-solid fa-chevron-right"></i>
+              </button>
 
-            <ul className="carousel__dots">
-                {pictures.map((_, index) => (
-                    <li
-                        key={index}
-                        className={`carousel__dot ${index === currentImageIndex ? "active" : ""}`}
-                    ></li>
-                ))}
-            </ul>
-        </div>
-        
-        <div className='logement-description__title'>
-            <h2>{logement.title}</h2>
-            <h3>{logement.location}</h3>
-        <div className='logement-description__tag-rate-container'>
-            <ul className="logement-description__tags">
-                {logement.tags.map((tag, index) => (
-                    <li key={index} className="tag">
-                        {tag}
-                    </li>
-                ))}
-            </ul>
-            <RatingStars rating={logement.rating} />
-        </div>
-        </div>
-    </div>
+              <ul className="carousel__dots">
+                  {pictures.map((_, index) => (
+                      <li
+                          key={index}
+                          className={`carousel__dot ${index === currentImageIndex ? "active" : ""}`}
+                      ></li>
+                  ))}
+              </ul>
+          </div>
+          
+          <div className='logement-description__title'>
+              <h2 className='logement-description__title2'>{logement.title}</h2>
+              <h3>{logement.location}</h3>
+
+              <div className='logement-description__tag-rate-container'>
+                  <ul className="logement-description__tags">
+                      {logement.tags.map((tag, index) => (
+                          <li key={index} className="tag">
+                              {tag}
+                          </li>
+                      ))}
+                  </ul>
+                  <RatingStars rating={logement.rating} />
+              </div>    
+          </div>
+
+          <div className='logement-description__host'>
+              <div className='logement-description__host__info'>
+                <div className='logement-description__host__name'>
+                  <p>{logement.host.name}</p>
+                </div>
+                <img
+                  src={logement.host.picture}
+                  alt={`Hôte ${logement.host.name}`}
+                  className="logement-description__host__picture"
+                />
+              </div>
+          </div>     
+      </div>
+
+      <div className="logement-description__collapses">
+      <Collapse title="Description" content={logement.description} />
+      <Collapse
+        title="Équipements" 
+        content={
+          <ul>
+            {logement.equipments.map((equipment, index) => (
+              <li key={index}>{equipment}</li>
+            ))}
+          </ul>
+        }
+      />
+      </div>
+    </>
   );
 }
 
