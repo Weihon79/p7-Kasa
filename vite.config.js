6b918c94ac80a9234c4f4cf036a1bed4d2108134
Weihon79 <ervan.chin@gmail.com>
2025-05-13 18:19:56 +0200

    correction chemin gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ajoute cette ligne pour spécifier la racine du projet
+  base: "/p7-Kasa", // Ajoute cette ligne pour spécifier la racine du projet
 });
