b6b758bdf0561414253af7bb69999e10f08205f2
Weihon79 <ervan.chin@gmail.com>
2025-05-13 18:13:15 +0200

    main jsx chemin

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import NotFound from './pages/notfound/NotFound';
-import LogementDescription from './pages/logementDescription/LogementDescription';
-import logements from './data/logements.json';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import NotFound from "./pages/notfound/NotFound";
+import LogementDescription from "./pages/logementDescription/LogementDescription";
+import logements from "./data/logements.json";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <Header />
-        <Routes> {/* On passe logements en props */}
-            <Route path="/" element={<Home logements={logements}/>} />
-            <Route path="/about" element={<About />} />
-            <Route path="/logement/:id" element={<LogementDescription logements={logements} />} />
-            <Route path="*" element={<NotFound />} />
+        <Routes>
+          {" "}
+          {/* On passe logements en props */}
+          <Route path="/" element={<Home logements={logements} />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/logement/:id"
+            element={<LogementDescription logements={logements} />}
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-        <Footer /> {/* Affichage du footer et header sur toutes les pages */}
+      <Footer /> {/* Affichage du footer et header sur toutes les pages */}
     </>
   );
 }
